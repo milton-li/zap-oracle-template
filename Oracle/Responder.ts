cd3d06e190a36f5f65d471b359f3f297bdcc3266
Milton Li <milton@aeglepalette.com>
2019-06-06 12:43:10 -0400

    Lower bonding curve

--- Oracle/Responder.ts
+++ Oracle/Responder.ts
@@ -1,10 +1,6 @@
 
 export async function getResponse(query:string,params:string[]|[]){
 	//Get data based on the query string and Parameters
-
-	console.log("QUERY: " + query);
-	console.log("PARAMS: " + params);
-
-	return ['response here']
+		return ['response here']
 
 }
